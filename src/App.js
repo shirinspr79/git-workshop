c645dd90412e0e389244af475d1846475d81f8d3
shirinspr79 <shirinspr79@gmail.com>
2022-11-11 15:13:19 +0330

    Import additional into the app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { CustomNavbar } from './Navbar';
 import './App.css';
 import Main from './Main';
+import Additional from './Additional';
 // import { Route, Routes} from 'react-router-dom'
 
 export default function App(){
@@ -10,6 +11,7 @@ export default function App(){
       
       <CustomNavbar />
       <Main />
+       <Additional />
       {/* <Routes> 
         <Route path='/' element={<CustomNavbar />} />
       </Routes>  */}
